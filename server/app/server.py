956f0cb121b403b055368cb58a69c4c48a417d3c
Vidhi0003 <148511985+Vidhi0003@users.noreply.github.com>
2025-04-05 07:26:23 -0500

    Create server.py

---
+++ server/app/server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# To allow CORS from our frontend ..
+CORS(app)
+
+@app.route("/question", methods=['GET'])
+def getTriviaQuestion():
+    questions = [
+        {'question' : 'When was WiCS founded?', 'answer': '2013', 'id':1},
+        {'question' : 'What class code is Advanced Data Structures?', 'answer': 'CS351', 'id':2},
+        {'question' : 'Where is the WiCS Lounge', 'answer': 'inside the CS Lounge', 'id':3},
+        {'question' : 'Who is the head of department of UIC CS', 'answer': 'Prof Bob Sloan', 'id':4},
+        {'question' : 'Who is WiCS Faculty Advisor', 'answer': 'Prof Shanon Reckinger', 'id':5},
+        {'question' : 'What is WiCS biggest event', 'answer': 'Scavenger Hunt', 'id':6},
+        {'question' : 'When was the first SparkHacks?', 'answer': '2023', 'id':7},
+        {'question' : 'Who is teaching CS361', 'answer': 'Prof Maratos', 'id':8}
+    ]
+
+    result = []
+    amount = request.args.get('amount')
+    if not amount:
+        amount = 1
+
+    for i in range(int(amount)):
+        result.append(questions[i])
+            
+    return result
+
+@app.route("/wics")
+def wics():
+    return 'hello wics'
+
+@app.route("/")
+def hello():
+    return 'hello world'
+        
